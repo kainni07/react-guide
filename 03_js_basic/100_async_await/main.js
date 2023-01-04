7a12b48de1821a6fb83314ef557697015a1263e5
kainni07 <kainnicapri@gmail.com>
2023-01-04 18:01:06 +0900

    非同期処理のawaitとasyncを追加

---
+++ 03_js_basic/100_async_await/main.js
@@ -0,0 +1,17 @@
+
+let a = 0;
+
+init();
+async function init() {
+  try {
+    const result = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        a = 1;
+        reject(a)
+      }, 2000);
+    })
+      console.log(result);
+  } catch(e) {
+    console.log('catchが実行', e)
+  }
+}
